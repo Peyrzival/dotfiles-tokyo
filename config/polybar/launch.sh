261cbfa7a39303fc8d6a055b18926992418a4744
Peyrzival <ricardodcfilho@protonmail.com>
2022-06-19 15:09:11 -0300

    happy

--- config/polybar/launch.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-killall -q polybar
-
-echo "---" | tee -a /tmp/polybar.log
-MONITOR="HDMI-0" polybar main 2>&1 | tee -a /tmp/polybar.log & disown
-MONITOR="DP-3" polybar main 2>&1 | tee -a /tmp/polybar.log & disown
-echo "Bars launched..."
